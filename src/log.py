e028d0ed882851d959888e595c72575f3394edcd
Peter Kofler <peter.kofler@code-cop.org>
2025-07-12 23:01:42 +0200

    Add another log.

--- src/log.py
+++ src/log.py
@@ -6,6 +6,7 @@ from datetime import datetime
 class Colors:
     GREEN = "\033[92m"
     YELLOW = "\033[93m"
+    RED = "\033[91m"
     CYAN = "\033[96m"
     RESET = "\033[0m"
 
@@ -19,6 +20,9 @@ class Log:
     def _write_start_time(self) -> None:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self._write("START", timestamp, Colors.CYAN)
+
+    def room(self, text: str) -> None:
+        self._write("ROOM", text, Colors.RED)
 
     def gameText(self, text: str) -> None:
         self._write("GAME", text, Colors.GREEN)

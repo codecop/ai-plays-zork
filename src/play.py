c37e82909281ee4464fa0ff486c17ff4190dc3b6
Peter Kofler <peter.kofler@code-cop.org>
2025-07-24 22:22:09 +0200

    Add throttling for fast answers.

--- src/play.py
+++ src/play.py
@@ -2,12 +2,18 @@ import sys
 from create_ai import create_ai
 from run import run
 
-
 # init AI
 if len(sys.argv) > 1:
     config = sys.argv[1]
 else:
-    raise ValueError("No config provided")
+    print("Usage: python src/play.py config [threshold_seconds]")
+    sys.exit(0)
+
+if len(sys.argv) > 2:
+    threshold_seconds = float(sys.argv[2])
+else:
+    threshold_seconds = 0
+
 
 ai = create_ai(config)
-run(ai)
+run(ai, threshold_seconds)
--- src/run.py
+++ src/run.py
@@ -1,8 +1,9 @@
 from ai_interface import AiInterface
 from game import Game
+import time
 
 
-def run(ai: AiInterface) -> None:
+def run(ai: AiInterface, threshold: float = 0) -> None:
     """Run the game loop with a given AI."""
 
     # start game
@@ -13,14 +14,21 @@ def run(ai: AiInterface) -> None:
     ai.start(game_notes, game_intro)
 
     # run loop
+    start_time = time.time()
     command = "look"
     while True:
         game_output = game.do_command(command)
         ai.log.game(game_output)
 
+        # wait for threshold
+        elapsed_time = time.time() - start_time
+        if elapsed_time < threshold:
+            time.sleep(threshold - elapsed_time)
+
+        start_time = time.time()
+
         # Get AI's next move
-        context = game_output
-        command = ai.get_next_command(context)
+        command = ai.get_next_command(game_output)
         ai.log.command(command)
 
         if game.game_ended():

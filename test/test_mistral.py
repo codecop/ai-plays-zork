f23e6359d58e26d0da73428c278b4d8a8530aa6f
Peter Kofler <peter.kofler@code-cop.org>
2025-08-05 23:47:06 +0200

    Sketch function calling with Mistral.

--- test/test_mistral.py
+++ test/test_mistral.py
@@ -1,3 +1,4 @@
+import json
 import os
 import pytest
 from mistralai import Mistral
@@ -36,10 +37,8 @@ def test_mistral_agent():
 
     simple_agent = client.beta.agents.create(
         model=model,
-        description="A simple Agent with persistent state.",
+        description="A unit test Agent with persistent state.",
         name="Test Agent",
-        # instructions="You have the ability to perform web searches with `web_search` to find up-to-date information.",
-        # tools=[{"type": "web_search"}],
     )
 
     response = client.beta.conversations.start(
@@ -47,9 +46,86 @@ def test_mistral_agent():
         inputs=[{"role": "user", "content": "Who is Albert Einstein? Just 1 sentence"}],
         # store=False
     )
+    assert len(response.outputs) == 1
     print(response.outputs[0].content)
 
     response = client.beta.conversations.append(
         conversation_id=response.conversation_id, inputs="Give another sentence."
     )
+    assert len(response.outputs) == 1
     print(response.outputs[0].content)
+
+
+def identify_user(user_id: str) -> str:
+    if user_id == "U128":
+        return "Hans"
+    return "Peter"
+
+
+# Try using tools, see https://docs.mistral.ai/capabilities/function_calling/
+def test_mistral_function_calling():
+    client = Mistral(api_key=api_key)
+
+    simple_agent = client.beta.agents.create(
+        model=model,
+        description="A unit test Agent with functions.",
+        name="Test Function Calling Agent",
+        tools=[
+            {
+                "type": "function",
+                "function": {
+                    "name": "identify_user",
+                    "description": "Get user name from user id",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "user_id": {
+                                "type": "string",
+                                "description": "The user id.",
+                            }
+                        },
+                        "required": ["user_id"],
+                    },
+                },
+            },
+        ],
+    )
+
+    response = client.beta.conversations.start(
+        agent_id=simple_agent.id,
+        inputs=[
+            {
+                "role": "user",
+                "content": 'Who is user U128? Return the sentence "User U128 is %s"',
+            }
+        ],
+    )
+    assert len(response.outputs) == 1
+    output = response.outputs[0]
+    # Union[ToolExecutionEntry, FunctionCallEntry, MessageOutputEntry, AgentHandoffEntry]
+    assert output.type == "function.call"
+
+    # eval function
+    function_name = output.name
+    function_args = json.loads(output.arguments)
+    assert function_name == "identify_user"
+    assert function_args == {"user_id": "U128"}
+    function_result = globals()[function_name](**function_args)
+    assert function_result == "Hans"
+
+    response = client.beta.conversations.append(
+        conversation_id=response.conversation_id,
+        # List[MessageInputEntry|FunctionResultEntry]
+        inputs=[
+            {
+                "tool_call_id": output.tool_call_id,
+                "result": function_result,
+            }
+        ],
+    )
+
+    assert len(response.outputs) == 1
+    output = response.outputs[0]
+    assert output.type == "message.output"
+    print(output.content)
+    assert output.content == "User U128 is Hans"

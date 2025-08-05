6ae92ff2f909167d58f888ce50539bd64c8e7ef1
Peter Kofler <peter.kofler@code-cop.org>
2025-08-05 23:28:36 +0200

    It's always 20 agents on Mistral, so it will not keep more than that

--- test/mistral_delete_agents.py
+++ test/mistral_delete_agents.py
@@ -8,6 +8,9 @@ model = "mistral-small-latest"  # free
 client = Mistral(api_key=api_key)
 
 agents = client.beta.agents.list()
+agents.sort(key=lambda agent: agent.created_at)
+print("found %s agents" % len(agents))
+# it's always 20 agents, so it will not keep more than that
 for agent in agents:
-    print("deleting " + agent.name)
+    print("deleting %s from %s" % (agent.name, agent.created_at))
     # client.beta.agents.delete(agent_id=agent.id)

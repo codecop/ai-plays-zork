94c05e5efe5800f55115589a06441cd38edad3e5
Peter Kofler <peter.kofler@code-cop.org>
2025-08-06 00:44:49 +0200

    Extract methods.

--- src/mistral_ai.py
+++ src/mistral_ai.py
@@ -32,6 +32,7 @@ class MistralAi(AiInterface):
         system_prompt = self.load_resource("system_prompt.md").format(
             game_notes=game_notes, game_intro=game_intro
         )
+        self.write_run_resource("system_prompt.md", system_prompt)
 
         self.agent = self.client.beta.agents.create(
             model=self.config()["model"],
@@ -41,6 +42,9 @@ class MistralAi(AiInterface):
             tools=self.config()["tools"],
         )
 
+        self._log_agent_info()
+
+    def _log_agent_info(self):
         self.log.ai(
             f"ai: {self.__class__}\n"
             + f"configuration: {self.configuration}\n"
@@ -49,10 +53,28 @@ class MistralAi(AiInterface):
             + f"name: {self.agent.name}\n"
             + f"description: {self.agent.description}"
         )
-        self.write_run_resource("system_prompt.md", system_prompt)
 
     def get_next_command(self, context: str) -> str:
         prompt = f"Game answers with {context}"
+        response = self._send_initial_prompt_to_server(prompt)
+
+        if not response.outputs or len(response.outputs) == 0:
+            self.log.ai("NO RESPONSE")
+            return "NO RESPONSE"
+
+        if len(response.outputs) > 1:
+            self.log.ai("WARN multiple responses " + str(response.outputs))
+
+        output = response.outputs[0]
+        if output.type == "message.output":
+            self.calls += 1
+            result = output.content
+            return self._extract_command_from(result)
+        else:
+            self.log.ai("WARN not a message response " + str(output))
+            return "NO RESPONSE"
+
+    def _send_initial_prompt_to_server(self, prompt: str) -> str:
         if not self.conversation_id:
             response = self.client.beta.conversations.start(
                 agent_id=self.agent.id,
@@ -64,25 +86,13 @@ class MistralAi(AiInterface):
                 conversation_id=self.conversation_id,
                 inputs=[{"role": "user", "content": prompt}],
             )
+        return response
 
-        self.calls += 1
-        if response.outputs and len(response.outputs) > 0:
-            if len(response.outputs) > 1:
-                self.log.ai("WARN multiple responses " + str(response.outputs))
-
-            output = response.outputs[0]
-            if output.type == "message.output":
-                result = output.content
-            else:
-                self.log.ai("WARN not a message response " + str(output))
-                return "NO RESPONSE"
-
-            if "\n" in result:
-                self.log.ai(result)
-                result = result.splitlines()[-1]
-            return result
-        self.log.ai("NO RESPONSE")
-        return "NO RESPONSE"
+    def _extract_command_from(self, response: str) -> str:
+        if "\n" in response:
+            self.log.ai(response)
+            response = response.splitlines()[-1]
+        return response
 
     def close(self) -> None:
         # currently no explicit cleanup needed

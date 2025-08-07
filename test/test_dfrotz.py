3406ff7f74d701da8c0181332f1a8d2c6580207c
Peter Kofler <peter.kofler@code-cop.org>
2025-08-07 14:12:58 +0200

    R: Use resource monitor.

--- test/test_dfrotz.py
+++ test/test_dfrotz.py
@@ -6,25 +6,27 @@ import subprocess
 def test_frotz_subprocess():
 
     frotz_path = os.path.expanduser("~/.pyfrotz/dfrotz")
-    frotz = subprocess.Popen(
+
+    with subprocess.Popen(
         [frotz_path, "-p", "data/zork1.z3"],
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         text=True,
         bufsize=1,
-    )
+    ) as frotz:
 
-    while True:
-        line = frotz.stdout.readline().rstrip()
-        print('> "' + line + '"', flush=True)
-        if line == "There is a small mailbox here.":
-            break
-    assert line == "There is a small mailbox here."
+        while True:
+            line = frotz.stdout.readline().rstrip()
+            print('> "' + line + '"', flush=True)
+            if line == "There is a small mailbox here.":
+                break
+        assert line == "There is a small mailbox here."
 
-    frotz.stdin.write("quit")
-    frotz.stdin.write("y")
-    frotz.stdin.flush()
+        # quit game
+        frotz.stdin.write("quit")
+        frotz.stdin.write("y")
+        frotz.stdin.flush()
 
     # close all resources
     frotz.stdin.close()

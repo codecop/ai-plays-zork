e979502997a94433a26e1b5e12ec8e986ce12ee9
Peter Kofler <peter.kofler@code-cop.org>
2025-09-02 20:15:23 +0200

    Test coverage Game.

--- test/test_game.py
+++ test/test_game.py
@@ -12,7 +12,21 @@ def test_game_formatting():
         + "You are standing in an open field west of a white house, with a boarded front door. "
         + "There is a small mailbox here."
     )
+    assert game.room_name() == "West of House"
 
     game.quit()
+    game.close()
 
+
+def test_game_errors():
+
+    game = Game()
+    assert game.get_intro() == ""
+
+    description = game.do_command("foo")
+    assert description == 'I don\'t know the word "foo".'
+
+    assert game.game_ended() is False
+
+    # no game.quit()
     game.close()

6a7153509d804ec3ba2cf61306ad697a837109df
Peter Kofler <peter.kofler@code-cop.org>
2025-09-02 21:15:57 +0200

    Add ports, works somehow in map.

--- src/graphviz_room_change.py
+++ src/graphviz_room_change.py
@@ -4,40 +4,47 @@ from map.exploration_action import ExplorationAction
 from room_change import RoomChange
 
 
+DIRECTION_PORTS = {
+    "north": ("n", "s"),
+    "east": ("e", "w"),
+    "south": ("s", "n"),
+    "west": ("w", "e"),
+    "northeast": ("ne", "sw"),
+    "southeast": ("se", "nw"),
+    "southwest": ("sw", "ne"),
+    "northwest": ("nw", "se"),
+}
+
+
 class GraphvizRoomChange(RoomChange):
     """Draw the map of visited new rooms using Graphviz."""
 
-    def __init__(self, run_folder: Path):
-        self.g = Digraph("G", filename=run_folder / "map.gv")
-        self.edges = set()
-        self.g_updated = False
+    def __init__(self, path: Path):
+        path.mkdir(parents=True, exist_ok=True)
+        filename = path / "map.gv"
+        self.g = Digraph("G", filename=filename)
+        self.known_edges = set()
+        self.is_graph_updated = False
 
     def record_movement(self, action: ExplorationAction) -> None:
-        edge = action.from_room_name + action.to_room_name + str(action.direction)
-        if edge not in self.edges:
-            self.edges.add(edge)
-            self.g.edge(
-                action.from_room_name, action.to_room_name, label=str(action.direction)
-            )
-            self.g_updated = True
+        edge = f"{action.from_room_name}-{action.to_room_name}-{action.direction}"
+        if edge not in self.known_edges:
+            self.known_edges.add(edge)
 
-            # maybe use ports? plus rank for layout... Code not tested
-            # # Direction to port mapping for proper compass arrows
-            # direction_ports = {
-            #     'north': ('s', 'n'), 'south': ('n', 's'),
-            #     'east': ('w', 'e'), 'west': ('e', 'w'),
-            #     'up': ('s', 'n'), 'down': ('n', 's')
-            # }
-
-            # if action.direction in direction_ports:
-            #     from_port, to_port = direction_ports[action.direction]
-            #     self.g.edge(f"{action.from_room_name}:{from_port}",
-            #                f"{action.to_room_name}:{to_port}",
-            #                label=action.direction)
-            # else:
-            #     self.g.edge(action.from_room_name, action.to_room_name, label=action.direction)
+            if action.direction in DIRECTION_PORTS:
+                from_port, to_port = DIRECTION_PORTS[action.direction]
+                self.g.edge(
+                    f"{action.from_room_name}:{from_port}",
+                    f"{action.to_room_name}:{to_port}",
+                    label=action.direction,
+                )
+            else:
+                self.g.edge(
+                    action.from_room_name, action.to_room_name, label=action.direction
+                )
+            self.is_graph_updated = True
 
     def display(self) -> None:
-        if self.g_updated:
+        if self.is_graph_updated:
             self.g.view()
-            self.g_updated = False
+            self.is_graph_updated = False

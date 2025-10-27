a3d6ae5bccd4994e2cc28cd0d3a5b72d9e7fa0cf
Peter Kofler <peter.kofler@code-cop.org>
2025-10-27 16:18:54 +0100

    Rename working dir folder name in code.

--- src/util/create_run.py
+++ src/util/create_run.py
@@ -7,11 +7,11 @@ from util.nice_log import NiceLog
 from util.file_utils import next_folder_name
 
 
-def create_run(config: str, scope="loop", base_path=".") -> Tuple[Path, Log]:
+def create_run(config: str, scope="loop", working_dir=".") -> Tuple[Path, Log]:
     """Create the run folder and log writing into it."""
 
     base_name = f"{scope}_{config}"
-    run_folder = next_folder_name(Path(base_path) / "runs", base_name)
+    run_folder = next_folder_name(Path(working_dir) / "runs", base_name)
     log = CompositeLog(NiceLog(run_folder), IndividualLog(run_folder, ""))
 
     return run_folder, log

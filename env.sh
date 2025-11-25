237a67f29491ef8f56784a4e5829cca3f6f7d99b
Lada Kesseler <archie.kesseler@gmail.com>
2025-11-25 10:30:19 -0500

    Pseudocode for the switch to pypthon virtual environment script

---
+++ env.sh
@@ -0,0 +1,5 @@
+1. set up python if there's no python in the path
+2. run `python --v` to make sure it's running
+3. if there's no .venv, then we create the environment via venv, and install dependencies from requirements.txt with pip
+4.  activate virtual environment
+5. do a pip list, so we know that the installation works

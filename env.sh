fcc492a1d7250ac4056f8e7afad299608e471fb5
Peter Kofler <peter.kofler@code-cop.org>
2025-11-25 17:04:37 +0100

    Add TODOs for next time.

--- env.sh
+++ env.sh
@@ -12,12 +12,22 @@ if [[ "$sourced" -eq 0 ]]; then
     exit 1
 fi
 
+# TODO set up Frotz, copy dfrotz
+ls -la ~/.pyfrotz
+python -m site
+target_folder=$LD_LIBRARY_PATH/python3.12/site-packages/frotz
+mkdir -p $target_folder
+cp ~/.pyfrotz/dfrotz $target_folder
+ls -la $target_folder
+
+# set up Python
 if ! command -v python &>/dev/null; then
     echo "python not found in PATH" >&2
     exit 1
 fi
 python --version
 
+# set up virtual environment
 if [[ ! -d .venv ]]; then
     python -m venv .venv
     source .venv/bin/activate
